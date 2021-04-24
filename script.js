93b3b18c042e7aebe8ad42b64120747d08c87eff
Carbolemons <34587311+Carbolemons@users.noreply.github.com>
2021-04-24 16:56:34 -0500

    done lol

---
+++ script.js
@@ -0,0 +1,16 @@
+//based on a pen by @robinselmer
+// includes a script from https://github.com/FoxInFlame/MinecraftColorCodes
+var url = "https://rest.miragefactions.com/votes";
+$(document).ready(function(){
+$.getJSON(url, function (rest) {
+  //data is the JSON string
+  var pl = "";
+  for (i = 0; 5 > i; i++) {
+    pl = pl + "<p class='head' style='object-fit: none;margin:0;'><img class='head' src='https://crafatar.com/renders/head/"+ rest[i].uuid + "?scale=2'></img> #"+(i+1)+" "+rest[i].PlayerName+"<br>Monthly Votes: "+rest[i].MonthTotal+"</p>";
+  }
+  $("#rest").html(
+      pl
+  );
+  $("#favicon").attr("src", rest.favicon);
+});
+});
